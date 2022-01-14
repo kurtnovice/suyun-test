b6fe6edc25d405989daa690fec127a19f9abade4
Amri Tjahjadi <amri.tjahjadi@gmail.com>
2022-01-15 01:36:29 +0700

    added telegram API

--- sender.py
+++ sender.py
@@ -1,4 +1,7 @@
 import abc
+from settings import TELEGRAM_TOKEN, CHAT_ID, TELEGRAM_URL
+import requests
+
 
 class Sender(metaclass=abc.ABCMeta):
 
@@ -23,8 +26,11 @@ class TelegramSender(Sender):
     _instance = None
 
     def __init__(self):
-        # setup configuration here, more than likely this will be a singleton too
+        # setup configuration here
+        # self._tb = telebot.TeleBot(TELEGRAM_TOKEN)
+        # tb.send_message(chatid, message)
         pass
+
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
@@ -38,17 +44,33 @@ class TelegramSender(Sender):
         val = transaction.get_dict()
         if transaction.status == "CONFIRMED":
             # simulate send by telegram API
-            print(f"Transaction {val['id']} type "\
+            message = f"Transaction {val['id']} type "\
                 f"{val['type'].upper()} has been {transaction.status} on "\
-                f"{transaction.date}")
-            return "12341345"
+                f"{transaction.date}"
+            print(message)
+
+            response_code = self._send(message, CHAT_ID)
+            return response_code
         else:
             return None
 
     def send_total(self, total_info):
-        print(f"Date: {total_info['date']}, Total In: {total_info['in']},"
-            f"Total Out: {total_info['out']}, Nett:{total_info['nett']}")
-        return "1232453245"
+        message = f"Date: {total_info['date']}, Total In: {total_info['in']},"\
+            f"Total Out: {total_info['out']}, Nett:{total_info['nett']}"
+        print(message)
+        response_code = self._send(message, CHAT_ID)
+        return response_code
 
+    def _send(self, message, chat_id):
+        body = {
+            "chat_id": chat_id,
+            "text": message
+        }
+        header = {
+            "Content-Type": "application/json"
+        }
+        url = f"{TELEGRAM_URL}bot{TELEGRAM_TOKEN}/sendMessage"
+        req = requests.request("POST", url=url, data=body)
+        return req.status_code
 
     
